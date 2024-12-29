d81de5490570c936b26fdce595a978948d0ad022
phon <hassanjaved186@gmail.com>
2024-12-30 04:30:48 +0500

    feat: add errors - todo: helper functions to return errors easily

---
+++ internal/models/return_errors.go
@@ -0,0 +1,7 @@
+package models
+
+import "github.com/labstack/echo/v4"
+
+// TODO create this package to return json for handlers e.g. ReturnInternalServerError
+
+func ReturnInternalServerError(ctx echo.Context) {}
